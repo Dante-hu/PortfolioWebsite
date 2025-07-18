c6a03727be2cb8b83b08f710c3265e3cdeb4baba
Dante-hu <dantercornelius@gmail.com>
2025-07-17 20:39:50 -0400

    Changed framer motion import statement

--- src/app/componets/HeroSection.jsx
+++ src/app/componets/HeroSection.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "motion/react";
 
 const HeroSection = () => {
   return (
